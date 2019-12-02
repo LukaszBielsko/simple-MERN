09e5b12367e891999b9ecccce05de59903e9bfa8
Lukasz Walczak <lukeabove@gmail.com>
2019-12-02 11:11:23 +0100

    heroku deployment changes to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const configDB = require('./config.js/keys')
+const path = require('path')
 
 const app= express()
 
@@ -24,6 +25,16 @@ mongoose.connect(db, {
 
 // use routes
 app.use('/api/items', items)
+
+// check if in production 
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 const port = process.env.PORT || 5000
 
